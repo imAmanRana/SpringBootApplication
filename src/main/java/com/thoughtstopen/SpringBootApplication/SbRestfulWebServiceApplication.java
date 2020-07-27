4970aedad90f58fcd472c788cc4984bf013062f2
Amandeep Singh <amandeep_rana@ymail.com>
2020-07-27 06:11:43 -0400

    Eureka Server Registration Demo

--- src/main/java/com/thoughtstopen/SpringBootApplication/SbRestfulWebServiceApplication.java
+++ src/main/java/com/thoughtstopen/SpringBootApplication/SbRestfulWebServiceApplication.java
@@ -2,8 +2,10 @@ package com.thoughtstopen.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SbRestfulWebServiceApplication {
 
 	public static void main(String[] args) {
